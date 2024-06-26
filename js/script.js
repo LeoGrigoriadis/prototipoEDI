b0407316bc7ec01dd37abb2016732cca1002d4ac
Leonel <leogrigo2014@gmail.com>
2024-06-26 01:28:08 -0300

    pantalla para seleccionar especialidad y horarios (incompleta).

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
-function test(){
+function desbloquearBoton(){
     let nombre = document.getElementById("nombre-completo");
     let documento = document.getElementById("nro-documento");
     let boton = document.getElementById("btn-busqueda");
+    
     if(nombre.value != "" && documento.value != ""){
         boton.classList.remove('disabled');
     } else {
         boton.classList.add('disabled');
     }
+}
+
+function guardarInfo(){
+    const nombre = document.getElementById("nombre-completo");
+    const documento = document.getElementById("nro-documento");
     
+    localStorage.setItem("nombre", nombre.value);
+    localStorage.setItem("documento", documento.value);
+}
+
+function setEspecialidad(htmlElement){
+    const buscador = document.getElementById("buscador-especialidades");
+    console.log(htmlElement.textContent)
+    buscador.value = htmlElement.textContent;
+    
+    localStorage.setItem("especialidad-elegida", htmlElement.value);
 }
